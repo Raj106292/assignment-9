86883950257538fca04baa738bfae651a55c2314
Raj106292 <rajdas.106292@gmail.com>
2022-10-11 03:57:01 +0600

    responsiveness done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
 
     const allQuizData = useLoaderData();
     const quizData = allQuizData.data;
-    console.log(quizData);
+
     return (
         <div className='my-4 mx-8'>
             <h2 className='text-3xl font-bold text-blue-500'>Welcome to Motion Quiz</h2>
-            <p className='mt-4 p-4 border-2 rounded-md'>Motion Quiz is a quiz website for the beginners of web developer. It provides you some valuable question of Important topics like: React, JavaScript, HTML, Github etc</p>
+            <p className='mt-4 p-4 border-2 rounded-md'>Motion Quiz is a quiz website for the beginners of web developer. It provides you some valuable question of Important topics like: React, JavaScript, HTML, Github etc.</p>
             <div className='quiz mt-6'>
                 {
                     quizData.map(quiz => <QuizData key={quiz.id} quiz={quiz}></QuizData>)
